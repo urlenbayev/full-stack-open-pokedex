024c45eb32c0b108a43ff6749083d8a971c7635b
AKBAR URLENBAYEV <91365919+urlenbayev@users.noreply.github.com>
2024-09-22 00:01:54 +0300

    11.6

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const express = require("express");
-const app = express();
+import dotenv from 'dotenv'
+import express from 'express'
+
+
+dotenv.config()
+
+
+const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+  console.log(`server started on port ${PORT}`)
+})
