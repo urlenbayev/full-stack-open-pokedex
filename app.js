617ea80f4674e8c013d42ca0036797c8d4ce28ce
AKBAR URLENBAYEV <91365919+urlenbayev@users.noreply.github.com>
2024-09-27 23:31:27 +0300

    11.12

--- app.js
+++ app.js
@@ -13,9 +13,17 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 
-app.get('/version', (req, res) => {
-  res.send('1.0')
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
 })
+
+
+app.get('/version', (req, res) => {
+  res.send('1.1')
+})
+
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
