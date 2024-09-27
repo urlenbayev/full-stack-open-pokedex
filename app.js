303893c25037382707161a8932aae4af1d8d2557
AKBAR URLENBAYEV <91365919+urlenbayev@users.noreply.github.com>
2024-09-27 21:55:52 +0300

    11.11

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+
+app.get('/version', (req, res) => {
+  res.send('1.0')
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
